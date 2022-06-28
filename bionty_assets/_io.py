328bab5be8938bdf02e162458895271a2e9671a0
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-28 15:13:25 +0200

    :memo: Add pickle io

--- bionty_assets/_io.py
+++ bionty_assets/_io.py
@@ -1,8 +1,21 @@
+import pickle
 from pathlib import Path
 from typing import Union
 
 import orjson
 from typeguard import typechecked
+
+
+@typechecked
+def read_pickle(filename: Union[str, Path]):
+    with open(filename, "rb") as handle:
+        return pickle.load(handle)
+
+
+@typechecked
+def write_pickle(data, filename: Union[str, Path]) -> None:
+    with open(filename, "wb") as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 @typechecked
