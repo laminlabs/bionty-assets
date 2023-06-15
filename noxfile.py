87cf6119174d4252f413085814bfb0606605a927
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-15 13:58:35 +0200

    👷 Update CI

--- noxfile.py
+++ noxfile.py
@@ -2,19 +2,24 @@ import nox
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
-nox.options.reuse_existing_virtualenvs = True
+nox.options.default_venv_backend = "none"
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session
+def install(session: nox.Session):
+    session.run(*"pip install .[dev,test]".split())
+    session.run(*"pip install lamindb[aws]".split())
+
+
+@nox.session()
 def build(session):
     login_testuser1(session)
-    session.install(".[dev,test]")
-    run_pytest(session)
+    run_pytest(session, coverage=False)
     build_docs(session)
     upload_docs_artifact()
     move_built_docs_to_docs_slash_project_slug()
