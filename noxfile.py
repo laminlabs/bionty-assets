33b9f34141561adca4e8d935cdaad0493bde2911
Lukas Heumos <lukas.heumos@posteo.net>
2024-01-24 02:20:36 -0800

    Add ICD 11 preparation script (#31)
    * Add ICD 11 preparation script
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * Add ICD 11 preparation script
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * Add ICD 11 preparation script
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * Python 3.11
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * laminci to dependencies
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * build
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * build
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * build
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * pre-commit
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * lamindb_estup
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * lamindb_estup
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * ICD to index
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * Remove stripout
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * Reset notebooks
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    ---------
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,4 @@
 import nox
-from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
 nox.options.default_venv_backend = "none"
@@ -10,16 +9,9 @@ def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
-@nox.session
-def install(session: nox.Session):
-    session.run(*"pip install .[dev,test]".split())
-    session.run(*"pip install lamindb[aws]".split())
-
-
 @nox.session()
 def build(session):
+    session.run(*"pip install -e .[dev]".split())
     login_testuser1(session)
-    run_pytest(session, coverage=False)
-    build_docs(session)
-    upload_docs_artifact()
-    move_built_docs_to_docs_slash_project_slug()
+    run_pytest(session)
+    build_docs(session, strict=True)
