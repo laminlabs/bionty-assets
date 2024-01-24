078834d9b39c2e4158076558081ac4c8fed82c64
zethson <lukas.heumos@posteo.net>
2024-01-24 10:31:36 +0100

    build
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,4 @@
 import nox
-from laminci import upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
 nox.options.default_venv_backend = "none"
@@ -10,14 +9,9 @@ def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
-@nox.session
-def install(session: nox.Session):
-    session.run(*"pip install .[dev]".split())
-
-
-@nox.session
+@nox.session()
 def build(session):
+    session.run(*"pip install -e .[dev]".split())
     login_testuser1(session)
-    run_pytest(session, coverage=False)
-    build_docs(session, strip_prefix=True, strict=True)
-    upload_docs_artifact(aws=True)
+    run_pytest(session)
+    build_docs(session, strict=True)
